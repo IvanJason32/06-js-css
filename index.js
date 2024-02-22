6495e85e470d3894b7777ff49855adede2cfc0b6
IvanJason134 <jestrada1@ucol.mx>
2024-02-21 23:28:36 -0600

    add todo

---
+++ index.js
@@ -0,0 +1,14 @@
+let numero = 42; // Tipo de dato: entero
+let flotante = 12.4 // Tipo de dato: flotante (no recuerdo si existen flotantes en javascript como tal)
+let cadena = "Hola, mundo!"; // Tipo de dato: cadena de texto o string
+let booleano = true; // Tipo de dato: booleano (puede ser true o false)
+let arreglo = [1, 2, 3, 4, 5]; // Tipo de dato: arreglo
+let objeto = { nombre: "Jason Ivan", edad: 23 }; // Tipo de dato: objeto conformado por una llave y un valor.
+
+
+console.log("Número: ", numero);
+console.log("Número flotante o decimal: ", flotante);
+console.log("Cadena: ", cadena);
+console.log("Booleano: ", booleano);
+console.log("Arreglo: ", arreglo);
+console.log("Objeto: ", objeto);
